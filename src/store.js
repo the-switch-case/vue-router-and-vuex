fa8032b575656441e9fd47c91d086e18882c4949
Felipe Bohnert Paetzold <felipe.paetzold@gmail.com>
2019-08-15 16:51:54 -0300

    concluindo o store

--- src/store.js
+++ src/store.js
@@ -3,23 +3,49 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
-const state = { todos: [] }
+const state = { todos: [], loading: false }
 
 const actions = {
     addTodo({ commit }, todo) {
+        commit('setLoading', true)
         return new Promise(resolve => {
             setTimeout(() => {
                 todo.id = Date.now()
                 commit('addTodo', todo)
+                commit('setLoading', false)
                 resolve(todo)
-            }, 1000)
+            }, 500)
         })
+    },
+
+    toggleTodo({ commit }, todo) {
+        commit('toggleTodo', todo)
+    },
+
+    removeTodo({ commit }, todo) {
+        commit('removeTodo', todo)
     }
 }
 
 const mutations = {
     addTodo(state, payload) {
         state.todos.push(payload)
+    },
+
+    setLoading(state, payload) {
+        state.loading = payload
+    },
+
+    toggleTodo(state, payload) {
+        const index = state.todos.findIndex(item => item.id === payload.id);
+        if (index > -1) {
+            const checked = !state.todos[index].checked;
+            Vue.set(state.todos, index, { ...state.todos[index], checked });
+        }
+    },
+
+    removeTodo(state, payload) {
+        state.todos = state.todos.filter(item => item.id !== payload.id)
     }
 }
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container grid-xs py-2">
-    <form @submit.prevent="addTodo(todo)">
+    <form @submit.prevent="add(todo)">
       <div class="input-group">
         <input type="text" v-model="todo.description" class="form-input" placeholder="Novo todo" />
         <button class="btn btn-primary input-group-btn" :class="{loading}">Adicionar</button>
@@ -14,42 +14,23 @@
 
 <script>
 import Todo from "@/components/Todo";
+import { mapActions, mapState } from "vuex";
 
 export default {
   name: "app",
   components: { Todo },
   data() {
-    return { todo: { checked: false }, loading: false };
+    return { todo: { checked: false } };
   },
   computed: {
-    todos() {
-      return this.$store.state.todos;
-    }
+    ...mapState(["todos", "loading"])
   },
   methods: {
-    async addTodo(todo) {
-      try {
-        this.loading = true;
-        await this.$store.dispatch("addTodo", todo);
-        this.todo = { checked: false };
-      } finally {
-        this.loading = false;
-      }
-    },
+    ...mapActions(["addTodo", "toggleTodo", "removeTodo"]),
 
-    toggleTodo(todo) {
-      const index = this.todos.findIndex(item => item.id === todo.id);
-      if (index > -1) {
-        const checked = !this.todos[index].checked;
-        this.$set(this.todos, index, { ...this.todos[index], checked });
-      }
-    },
-
-    removeTodo(todo) {
-      const index = this.todos.findIndex(item => item.id === todo.id);
-      if (index > -1) {
-        this.$delete(this.todos, index);
-      }
+    async add(todo) {
+      await this.addTodo(todo);
+      this.todo = { checked: false };
     }
   }
 };
